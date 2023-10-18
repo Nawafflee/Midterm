cfbe85ac72ef85b05712b50b2c4dcd403f018b76
Nawafflee <nawafrraheem@gmail.com>
2023-10-18 12:08:49 -0400

    MovieStore Constructor Created

---
+++ movieStore.py
@@ -0,0 +1,12 @@
+
+
+class MovieStore:
+
+    def __init__(self,store_name,total_number_of_movies_available,rental_fee_per_movie,late_fee_per_day):
+            self._storeName = store_name
+            self._totalNumberOfMovies = total_number_of_movies_available
+            self._numberOfAvailableMovies = total_number_of_movies_available
+            self._rentalFeePerMovie = rental_fee_per_movie
+            self._lateFeePerDay = late_fee_per_day
+
+    
