40550b63eecd7751e7b77f2fa030e607a7686a1d
Nawafflee <nawafrraheem@gmail.com>
2023-10-18 12:23:02 -0400

    Created Accessors and mutator for event attributes

--- movieStore.py
+++ movieStore.py
@@ -2,6 +2,7 @@
 
 class MovieStore:
 
+    #constructor
     def __init__(self,store_name,total_number_of_movies_available,rental_fee_per_movie,late_fee_per_day):
             self._storeName = store_name
             self._totalNumberOfMovies = total_number_of_movies_available
@@ -9,4 +10,38 @@ class MovieStore:
             self._rentalFeePerMovie = rental_fee_per_movie
             self._lateFeePerDay = late_fee_per_day
 
+
+    #Getters
+    def getStoreName(self):
+        return self._storeName
+    
+    def getTotalNumberOfMovies(self):
+        return self._totalNumberOfMovies
+    
+    def getNumberOfAvailableMovies(self):
+        return self._numberOfAvailableMovies
+    
+    def getRentalFeePerMovie(self):
+        return self._rentalFeePerMovie
+    
+    def getLateFeePerDay(self):
+        return self._lateFeePerDay
+    
+    #setters
+    def setStoreName(self,storeName):
+        self._storeName = storeName
+    
+    def setTotalNumberofMovies(self,totalNumberOfMovies):
+        self._totalNumberOfMovies = totalNumberOfMovies
+    
+    def setNumberOfAvailableMovies(self,numberOfAvailableMovies):
+        self._numberOfAvailableMovies = numberOfAvailableMovies
+    
+    def setRentalFee(self,rentalFeePermovie):
+        self._rentalFeePerMovie = rentalFeePermovie
+    
+    def setLateFeePerDay(self,lateFeePerDay):
+        self._lateFeePerDay = lateFeePerDay
+
+    
     
